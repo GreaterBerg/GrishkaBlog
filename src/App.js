34a9e73b9d7c092c7c5da06df720a1ea2b5ab625
Astemir <125074932+GreaterBerg@users.noreply.github.com>
2025-05-07 22:02:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import BlogBlock from './components/blog_block/BlogBlock';
+import Amik from './photo_2025-05-07_21-50-13.jpg'
 
 
 function App() {
   return (
     <div>
-      <img src="./photo_2025-05-07_21-50-13.jpg"/>
+      <img src="{Amik}" alt="Amik"/>
     </div>
     
   );
