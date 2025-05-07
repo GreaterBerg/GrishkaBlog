1ed47f881f08ce35b5e9446f20262005a34e3871
Astemir <125074932+GreaterBerg@users.noreply.github.com>
2025-05-07 21:57:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,31 +8,7 @@ import BlogBlock from './components/blog_block/BlogBlock';
 function App() {
   return (
     <div>
-      /*<Navbar />
-      <BlogBlock 
-      category="новости"
-      backgroundImage="https://cdn.discordapp.com/attachments/981615157878874152/1179112258769530932/namroud-gorguis-FZWivbri0Xk-unsplash_1.jpg?ex=657898dd&is=656623dd&hm=99633b2b5f17e78d75184257bed8c36fbeeb9cd313d12706d13c68a41ac2d4c3&"
-      title="темир псеунов атлет в апекс легенды"
-      date="29.11.2023"
-      link="#"
-      />
-      <Analytics />
-      <BlogBlock 
-      category="блог"
-      backgroundImage="https://cdn.discordapp.com/attachments/981615157878874152/1179112258769530932/namroud-gorguis-FZWivbri0Xk-unsplash_1.jpg?ex=657898dd&is=656623dd&hm=99633b2b5f17e78d75184257bed8c36fbeeb9cd313d12706d13c68a41ac2d4c3&"
-      title="телеграм подписончик оформляхтунг"
-      date="29.11.2023"
-      link="#"
-      />
-      <BlogBlock 
-      category="технологии"
-      backgroundImage="https://avatars.mds.yandex.net/get-lpc/1520633/4fa460e8-3cfd-4de3-b40f-1f4e6cfc60b7/orig"
-      title="умни дом яндекс алисы все приколы"
-      date="29.11.2023"
-      link="#"
-      />
-      */
-      <img src="photo_2025-05-07_21-50-13.jpg"/>
+      <img src="./photo_2025-05-07_21-50-13.jpg"/>
     </div>
     
   );
