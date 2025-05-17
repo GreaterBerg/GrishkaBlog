76884f21b5ceba1cf40f2196ad8d3cc261808db3
Astemir <125074932+GreaterBerg@users.noreply.github.com>
2025-05-17 22:46:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import temik from './photo_2025-05-07_22-22-23.jpg';
 import irina from './photo_2025-05-07_22-22-18.jpg';
 import temir2 from './photo_2025-04-13_20-32-33.png';
 import zombieferma from './photo_2025-05-12_22-44-45.jpg';
-import spiderVideo from './video_2025-05-17_20-04-34.mp4';
+import spiderman from './video_2025-05-17_20-04-34.mp4';
 
 
 
@@ -26,7 +26,9 @@ function App() {
       <img src={temik} alt="Amik" class="Amik"/>
       <img src={irina} alt="Amik" class="Amik"/>
       <img src={temir2} alt="Amik" class="Amik"/>
-      <video src={spiderVideo} type="video/mp4"></video>
+      <video width="750" height="500" controls >
+      <source src={spiderman} type="video/mp4"/>
+     </video>
     
   );
 }
