b659711a74c2e881b17f9a3cc946bf408e120fc4
Astemir <125074932+GreaterBerg@users.noreply.github.com>
2025-05-17 22:40:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,9 @@ function App() {
       <img src={temik} alt="Amik" class="Amik"/>
       <img src={irina} alt="Amik" class="Amik"/>
       <img src={temir2} alt="Amik" class="Amik"/>
-      <video loop autoplay muted preload="auto" class="Amik" src={spiderVideo}/>
+      <video loop autoplay muted preload="auto" class="Amik"}>
+      <source src={spiderVideo} type="video/mp4">
+  </video>
     
   );
 }
