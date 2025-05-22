651be53027bb5874b671f72ec0844ae2255dea1c
grisha <125074932+GreaterBerg@users.noreply.github.com>
2025-05-22 15:10:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,52 +8,52 @@ import spiderman from './video_2025-05-17_20-04-34.mp4';
 import moneyguy from './8979097226ecf3d04cee34438ffc48f5.jpg';
 
 import angelinka from './angelinka.mp4';
-import angelinkaPoster from './ангелинкагелик.png';
+import angelinkaPoster from './photo_2025-05-22_15-00-16.jpg';
 
 import danachi from './IMG_1041.MP4';
-import danachiPoster from './ангелинкагелик.png';
+import danachiPoster from './photo_2025-05-22_15-00-22.jpg';
 
 import emma from './emma.mp4';
-import emmaPoster from './ангелинкагелик.png';
+import emmaPoster from './photo_2025-05-22_15-00-15.jpg';
 
 import temik from './temik.MP4';
-import temikPoster from './ангелинкагелик.png';
+import temikPoster from './photo_2025-05-22_15-02-13.jpg';
 
 import madin from './madik.mp4';
-import madinPoster from './ангелинкагелик.png';
+import madinPoster from './photo_2025-05-22_15-00-34.jpg';
 
 import abrek from './abrek.mp4';
-import abrekPoster from './ангелинкагелик.png';
+import abrekPoster from './photo_2025-05-22_15-00-23.jpg';
 
 import azak from './azak.MP4';
-import azakPoster from './ангелинкагелик.png';
+import azakPoster from './photo_2025-05-22_15-00-17.jpg';
 
 import azac from './azac.MP4';
-import azacPoster from './ангелинкагелик.png';
+import azacPoster from './photo_2025-05-22_15-00-20.jpg';
 
 import amik from './amir.mp4';
-import amikPoster from './ангелинкагелик.png';
+import amikPoster from './photo_2025-05-22_15-00-31.jpg';
 
 import danam from './makayo.mp4';
-import danamPoster from './ангелинкагелик.png';
+import danamPoster from './photo_2025-05-22_15-09-53.jpg';
 
 import kantik from './kantik.mp4';
-import kantikPoster from './ангелинкагелик.png';
+import kantikPoster from './photo_2025-05-22_15-00-36.jpg';
 
 import madina from './madina.MP4';
-import madinaPoster from './ангелинкагелик.png';
+import madinaPoster from './photo_2025-05-22_15-00-29.jpg';
 
 import milana from './milana.mp4';
-import milanaPoster from './ангелинкагелик.png';
+import milanaPoster from './photo_2025-05-22_15-09-54.jpg';
 
 import sveta from './sveta.mp4';
-import svetaPoster from './ангелинкагелик.png';
+import svetaPoster from './photo_2025-05-22_15-00-28.jpg';
 
 import temir from './temir.mp4';
-import temirPoster from './ангелинкагелик.png';
+import temirPoster from './photo_2025-05-22_15-00-24.jpg';
 
 import shamil from './shamil (2).MP4';
-import shamilPoster from './ангелинкагелик.png';
+import shamilPoster from './photo_2025-05-22_15-00-33.jpg';
 
 
 function App() {
@@ -63,63 +63,63 @@ function App() {
       <source src={angelinka} type="video/mp4"/>
      </video>
     
-      <video class="Amik" controls poster={angelinkaPoster} playsinline>
+      <video class="Amik" controls poster={danachiPoster} playsinline>
       <source src={danachi} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={emmaPoster} playsinline>
       <source src={emma} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={temikPoster} playsinline>
       <source src={temik} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={madinPoster} playsinline>
       <source src={madin} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={abrekPoster} playsinline>
       <source src={abrek} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={azakPoster} playsinline>
       <source src={azak} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={azacPoster} playsinline>
       <source src={azac} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={amikPoster} playsinline>
       <source src={amik} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={danamPoster} playsinline>
       <source src={danam} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={kantikPoster} playsinline>
       <source src={kantik} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={madinaPoster} playsinline>
       <source src={madina} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={milanaPoster} playsinline>
       <source src={milana} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={svetaPoster} playsinline>
       <source src={sveta} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={temirPoster} playsinline>
       <source src={temir} type="video/mp4"/>
      </video>
 
-    <video class="Amik" controls poster={angelinkaPoster} playsinline>
+    <video class="Amik" controls poster={shamilPoster} playsinline>
       <source src={shamil} type="video/mp4"/>
      </video>
     </div>
